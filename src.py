206f2ff7e422a57828bc691c0a294d8d957def49
522489 <Romain.monnoyer@hotmail.com>
2025-03-14 17:08:21 +0100

    feat(diff func): difference function
    add diff function

--- src.py
+++ src.py
@@ -5,7 +5,15 @@ def test_add():
     assert 4 == add(3, 1)
 def main():
     print("hello from main")
+
+def diff(x, y):
+    return x-y
+def test_diff():
+    assert 2 == diff(7,5)
+    assert 2 != diff(5,7)
+
 if __name__ == "__main__":
     print("hello there!")
     print("hello from Romain")
-    main()
+    print("1 + 1 = {}".format(add(1,1)))
+    print("1 - 1 = {}".format(diff(1,1)))
